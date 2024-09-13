f6a06fbc122e099c61c711da0ab774115309bfd2
N-jongsik <jongsik0625@naver.com>
2024-09-13 11:48:27 +0900

    react-redux

--- 04react/reactaxios/src/App.js
+++ 04react/reactaxios/src/App.js
@@ -1,20 +1,116 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import LayoutComp from "./layout/LayoutComp";
-import HomeComp from "./components/home/HomeComp";
-import AboutComp from "./components/AboutComp";
-import ProductComp from "./components/ProductComp";
+// import React from "react";
+// import { Route, Routes } from "react-router-dom";
+// import LayoutComp from "./layout/LayoutComp";
+// import HomeComp from "./components/home/HomeComp";
+// import AboutComp from "./components/AboutComp";
+// import ProductComp from "./components/ProductComp";
+
+// function App() {
+//   return (
+//     <>
+//       <Routes>
+//         <Route path="/" element={<LayoutComp />}>
+//           <Route index element={<HomeComp />} />
+//           <Route path="/about" element={<AboutComp />} />
+//           <Route path="/products" element={<ProductComp />} />
+//         </Route>
+//       </Routes>
+//     </>
+//   );
+// }
+
+// export default App;
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const listCnt = 10; //한 페이지에 보여지는 데이터 수
+  const pagerCnt = 5; //한 페이지에 보여지는 페이지 수
+
+  const [data, setData] = useState([]);
+  const [totalPage, setTotalpage] = useState(1);
+  const [currentpage, setCurrentpage] = useState(1);
+
+  const api = () => {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      console.log(res.data);
+      setData(res.data);
+
+      console.log("totalData = " + res.data.length);
+      console.log("totalPage = " + res.data.length / listCnt);
+
+      setTotalpage(res.data.length / listCnt);
+    });
+  };
+
+  useEffect(() => {
+    api();
+  }, []);
+
+  //밑에 페이지 숫자 보여주는 부분
+  const pageViewNumber = () => {
+    const pageNumbers = [];
+    const startPage = Math.floor(((currentpage - 1) / pagerCnt) * pagerCnt + 1);
+    const endPage = Math.min(startPage + pagerCnt - 1, totalPage);
+
+    for (let i = startPage; i <= endPage; i++) {
+      pageNumbers.push(
+        <span
+          onClick={() => setCurrentpage(i)}
+          style={{
+            cursor: "pointer",
+            margin: "0 5px",
+            color: currentpage === i ? "red" : "black",
+          }}
+        >
+          {i}
+        </span>
+      );
+    }
+    return pageNumbers;
+  };
+
+  //현재 페이지의 데이터들 보여주는 부분
+  const currentpageData = () => {
+    const startIndex = (currentpage - 1) * listCnt; //시작 인덱스는 현재 페이지 -1 * 한 페이지 데이터 갯수
+    const endIndex = startIndex + listCnt; //마지막 인덱스는 시작 인덱스 + 한페이지 데이터갯수
+    return data.slice(startIndex, endIndex);
+  };
+
   return (
     <>
-      <Routes>
-        <Route path="/" element={<LayoutComp />}>
-          <Route index element={<HomeComp />} />
-          <Route path="/about" element={<AboutComp />} />
-          <Route path="/products" element={<ProductComp />} />
-        </Route>
-      </Routes>
+      <div>
+        {currentpageData().map((item, i) => {
+          return (
+            <div>
+              <div key={i}>
+                {item.userId} / {item.title}
+              </div>
+            </div>
+          );
+        })}
+        <div style={{ fontSize: "2em" }}>{pageViewNumber()}</div>
+        <div>
+          {currentpage > 1 && (
+            <button
+              onClick={() => {
+                setCurrentpage(currentpage - 1);
+              }}
+            >
+              이전
+            </button>
+          )}
+          {currentpage < totalPage && (
+            <button
+              onClick={() => {
+                setCurrentpage(currentpage + 1);
+              }}
+            >
+              다음
+            </button>
+          )}
+        </div>
+      </div>
     </>
   );
 }
--- 04react/reacttail1/src/App.js
+++ 04react/reacttail1/src/App.js
@@ -1,35 +1,51 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
+
+// function App() {
+//   const [data, setData] = useState([]);
+//   const [num, setNum] = useState(2);
+
+//   const onClick = () => {
+//     axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+//       console.log(res.data);
+//       setData(res.data);
+//     });
+//   };
+
+//   useEffect(() => {
+//     axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+//       console.log(res.data);
+//       setData(res.data);
+//     });
+//   }, [num]);
+
+//   return (
+//     <div>
+//       <button onClick={onClick}>클릭</button>
+//       {data?.map((item, i) => {
+//         return (
+//           <div key={i}>
+//             {item.id} / {item.title}
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from "react";
+import NavbarComp from "./layout/NavbarComp";
+import VisualComp from "./components/home/VisualComp";
+import SectionComp from "./components/home/SectionComp";
 
 function App() {
-  const [data, setData] = useState([]);
-  const [num, setNum] = useState(2);
-
-  const onClick = () => {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
-      console.log(res.data);
-      setData(res.data);
-    });
-  };
-
-  useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
-      console.log(res.data);
-      setData(res.data);
-    });
-  }, [num]);
-
   return (
-    <div>
-      <button onClick={onClick}>클릭</button>
-      {data?.map((item, i) => {
-        return (
-          <div key={i}>
-            {item.id} / {item.title}
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <NavbarComp />
+      <VisualComp />
+      <SectionComp />
+    </>
   );
 }
 
